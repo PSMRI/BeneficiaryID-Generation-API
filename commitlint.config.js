01a6e7cd4c6e2f1c894566c4e142a01506559789
DurgaPrasad-54 <prasad8790237@gmail.com>
2026-03-05 10:21:05 +0530

    chore(workflow): enforce scoped commit message format

--- commitlint.config.js
+++ commitlint.config.js
@@ -6,6 +6,7 @@ module.exports = {
       ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore']
     ],
     'type-case': [2, 'always', 'lower-case'],
+    'scope-empty': [2, 'never'],
     'subject-empty': [2, 'never'],
     'subject-case': [0]
   }
